cf023aa28de874af4da8beeb078765d06e810c38
x14119641 <daniel.gil.romero24@gmail.com>
2020-10-14 20:06:32 +0100

    Items nows fits in the screen when minimize screen, added projects html

--- app/schema.sql
+++ app/schema.sql
@@ -19,3 +19,16 @@ CREATE TABLE projects (
   web TEXT,
   FOREIGN KEY (author_id) REFERENCES users (id)
 );
+
+-- Just for check that functionality works, this need to be deleted !! --
+INSERT INTO users (username, password, email)
+VALUES
+  ('admin', 'admin', 'admin@testing.com');
+
+INSERT INTO projects (title, body, author_id, created, repo, tags, web)
+VALUES
+  ('test title', 'test body description', 1, '2020-01-01 00:00:00', 'test_repo@github.com', 'python, sql', 'www.google.com'),
+  ('other title', 'other body description', 1, '2020-01-02 00:00:00', 'other_repo@github.com', 'python, aws', ''),
+   ('other title', 'other body description', 1, '2020-01-02 00:00:00', 'other_repo@github.com', 'python, aws', ''),
+    ('other title', 'other body description', 1, '2020-01-02 00:00:00', 'other_repo@github.com', 'python, aws', ''),
+     ('other title', 'other body description', 1, '2020-01-02 00:00:00', 'other_repo@github.com', 'python, aws', '');
--- app/views.py
+++ app/views.py
@@ -11,3 +11,16 @@ bp = Blueprint('views', __name__, template_folder='templates')
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/projects')
+def projects():
+    db = get_db()
+    data = db.execute(
+        'SELECT p.id, title, body, created, author_id, repo, tags, web'
+        ' FROM projects p JOIN users u ON p.author_id = u.id'
+        ' WHERE username = "admin"'
+    ).fetchall()
+    print(data)
+    for d in data:
+        print('# ', d)
+    return render_template('projects/projects.html', data=data)
